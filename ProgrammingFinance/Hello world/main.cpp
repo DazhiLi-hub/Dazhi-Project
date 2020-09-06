4bb1c80fec4fb83e62846bf221aa83e8aeb5ef0a
DazhiLi-hub <61103944+DazhiLi-hub@users.noreply.github.com>
2020-09-05 21:57:37 -0400

    C++ first class

---
+++ ProgrammingFinance/Hello world/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Hello world
+//
+//  Created by Dazhi Li on 9/5/20.
+//  Copyright © 2020 Dazhi Li. All rights reserved.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
